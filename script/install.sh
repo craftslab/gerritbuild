5be38f894c9ffa7577ed5150327e55ca3afee34f
Jia <49358172+craftslab@users.noreply.github.com>
2021-09-07 00:01:38 +0800

    Fix ci error

--- script/install.sh
+++ script/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+sudo apt install apt-transport-https curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
 sudo apt update
 sudo apt install -y bazel
-sudo apt install -y zip
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
