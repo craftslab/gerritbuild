22a1d675dae3976ed42af20a87d123ecbcdc496d
Jia <49358172+craftslab@users.noreply.github.com>
2021-09-07 00:13:59 +0800

    Add support to set tag in scripts

--- script/build.sh
+++ script/build.sh
@@ -1,45 +1,12 @@
 #!/bin/bash
 
-# See: https://gerrit-review.googlesource.com/Documentation/dev-bazel.html
-# See: https://gerrit-review.googlesource.com/Documentation/dev-release.html
-
-# Install Ant
-#sudo apt-get install ant
-
-# Install Apache2 (optional)
-#sudo apt-get install --reinstall apache2
-
-# Install Bazel
-# See: https://docs.bazel.build/versions/master/install-ubuntu.html
-# NOTICE: Bazel 0.8.1 required for building Gerrit 3.x.x and above.
-#sudo apt-get upgrade bazel
-
-# Install Maven
-#sudo apt install maven
-
-# Install Node.js
-#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-
-# Install Zip
-#sudo apt install zip
-
-# Install Python2.7
-#sudo apt update
-#sudo apt install python2.7
-#sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-#sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
-#sudo update-alternatives --config python
-#sudo update-alternatives --list python
-#python -V
-
 # Build Gerrit
 # Download: $HOME/.gerritcodereview
 # Release: /path/to/gerrit/bazel-bin/release.war
-version=$1
+version=${RELEASE_VERSION}
 ./tools/version.py $version
-git tag -m "v$version" "v$version"
-git submodule foreach git tag -m "v$version" "v$version"
+git tag -m "$version" "$version"
+git submodule foreach git tag -m "$version" "$version"
 bazel build release
 
 # Build Plugins (optional)
--- script/download.sh
+++ script/download.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-TAG=$1
+TAG=${RELEASE_VERSION}
 
 git clone --recurse-submodules https://gerrit.googlesource.com/gerrit
 
 pushd gerrit
-git checkout --recurse-submodules v$TAG
+git checkout --recurse-submodules $TAG
 popd
-
-#git clone --recurse-submodules https://gerrit.googlesource.com/gerrit --depth 1
-#
-#pushd source/gerrit
-#
-#git checkout master
-#git pull origin master
-#git pull -t origin master
-#
-#git submodule foreach git checkout master
-#git submodule foreach git pull origin master
-#git submodule foreach git pull -t origin master
-#
-#git checkout tags/v$TAG -b $TAG
-#git submodule foreach git checkout tags/v$TAG -b $TAG
-#
-#popd
