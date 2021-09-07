2df646d2ef55f1276c149dbceb0505739b47218c
Jia <49358172+craftslab@users.noreply.github.com>
2021-09-07 19:27:39 +0800

    FIx ci error

--- script/download.sh
+++ script/download.sh
@@ -2,6 +2,9 @@
 
 TAG=${RELEASE_VERSION}
 
+git config --global user.email "angersax@sina.com"
+git config --global user.name "Jia Jia"
+
 git clone --recurse-submodules https://gerrit.googlesource.com/gerrit
 
 pushd gerrit || exit
